aa03560c34a4e1e0745cf339395d29d37357d84a
Alejandro Aguilar <aaguilar2@hou-us05186-mbp13.attlocal.net>
2019-05-28 22:37:16 -0500

    after lecture code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 console.log('%c Hello, World!', 'color: orange')
+
+const container = document.querySelector("#container")
+
+// const para= document.createElement('p')
+// const text = document.createTextNode("Hello There")
+
+// para.appendChild(text)
+
+// container.appendChild(para)
+
+dankMemes.forEach(meme => {
+    const image = document.createElement('img')
+    image.src = meme
+    container.appendChild(image)  
+});
+
+
